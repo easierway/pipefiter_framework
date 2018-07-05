9ac2b7f65e8da207de27598bc089335d402ffdc2
hatlonely <hatlonely@gmail.com>
2018-07-05 18:20:28 +0800

    fix golint and add straight_pipeline_with_walltime

--- pipefilter/filter.go
+++ pipefilter/filter.go
@@ -1,12 +1,11 @@
-/*Package pipefilter is to define the interfaces and the structures for pipe-filter style implementation
-
-  Created: 2018-1-19
-  @All the copy rights belong to Mobvista.com
-**/
+// Package pipefilter is to define the interfaces and the structures for pipe-filter style implementation
+//
+//  Created: 2018-1-19
+//  @All the copy rights belong to Mobvista.com
 package pipefilter
 
-//Filter interface is the definition of the data processing components
-//Pipe-Filter structure
+// Filter interface is the definition of the data processing components
+// Pipe-Filter structure
 type Filter interface {
 	Process(data interface{}) (interface{}, error)
 }
--- pipefilter/straight_pipeline_test.go
+++ pipefilter/straight_pipeline_test.go
@@ -7,40 +7,32 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-type P1Filter struct {
-}
+type P1Filter struct{}
 
-func (self *P1Filter) Process(data interface{}) (interface{}, error) {
+func (f *P1Filter) Process(data interface{}) (interface{}, error) {
 	ret := *(data.(*string)) + "_P1"
 	return &ret, nil
 }
 
-type P2Filter struct {
-}
+type P2Filter struct{}
 
-func (self *P2Filter) Process(data interface{}) (interface{}, error) {
+func (f *P2Filter) Process(data interface{}) (interface{}, error) {
 	ret := *(data.(*string)) + "_P2"
 	return &ret, nil
 }
 
-type ErrorFilter struct {
-}
+type ErrorFilter struct{}
 
-var FailedProcessError error = errors.New("failed to process")
+var ErrFailedProcess = errors.New("failed to process")
 
-func (self *ErrorFilter) Process(data interface{}) (interface{}, error) {
+func (f *ErrorFilter) Process(data interface{}) (interface{}, error) {
 	ret := *(data.(*string)) + "_ERR"
-	return &ret, FailedProcessError
+	return &ret, ErrFailedProcess
 }
 
 func TestHappyProcess(t *testing.T) {
 	Convey("Given a pipeline", t, func() {
-		filters := []Filter{&P1Filter{}, &P2Filter{}}
-
-		std := StraightPipeline{
-			Name:    "Standard",
-			Filters: &filters,
-		}
+		std := NewStraightPipeline("Standard", &P1Filter{}, &P2Filter{})
 
 		Convey("When all the filters run successfully", func() {
 			in := "Start"
@@ -48,31 +40,22 @@ func TestHappyProcess(t *testing.T) {
 			Convey("Then get the excepted result", func() {
 				So(err, ShouldBeNil)
 				So("Start_P1_P2", ShouldEqual, *(ret.(*string)))
-
 			})
 		})
 	})
-
 }
 
 func TestUnhappyProcess(t *testing.T) {
 	Convey("Given a pipeline", t, func() {
-		filters := []Filter{&P1Filter{}, &ErrorFilter{}, &P2Filter{}}
-
-		std := StraightPipeline{
-			Name:    "Standard",
-			Filters: &filters,
-		}
+		std := NewStraightPipeline("Standard", &P1Filter{}, &ErrorFilter{}, &P2Filter{})
 
 		Convey("When some filters fail to run", func() {
 			in := "Start"
 			ret, err := std.Process(&in)
 			Convey("Then get the excepted error", func() {
-				So(err, ShouldEqual, FailedProcessError)
+				So(err, ShouldEqual, ErrFailedProcess)
 				So("Start_P1_ERR", ShouldEqual, *(ret.(*string)))
-
 			})
 		})
 	})
-
 }
